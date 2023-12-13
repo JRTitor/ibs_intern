8d6b608c698213062ca37e39a5605974627e25d6
JRTitor <lokk777@gmail.com>
2023-12-14 00:14:51 +0300

    added better version of DAG

--- airflow_task/table2v2.py
+++ airflow_task/table2v2.py
@@ -7,7 +7,9 @@
 чтобы посмотреть на http://localhost:8080/ как всё красиво работает
 
 буду благодарен, если порекомендуете статьи, где это просто и
-быстро описанно.
+быстро описанно
+
+здесь я использовал глоблаьные переменные, чтобы не открывать всё по новой и код выглядел логичнее
 '''
 
 from airflow import DAG
@@ -15,65 +17,64 @@ from airflow.operators.python import PythonOperator
 import pandas as pd
 from datetime import datetime
 from datetime import timedelta
-import os
 
+table1 = None
+table2 = None
+merged_table = None
 
 '''
 1) Загружает данные из таблицы public.flight_delays (или файла flight_delays.csv)
 '''
-def load_fights(flight_path:str='flight_delays.csv') -> pd.DataFrame:
+def load_fights(flight_path:str='flight_delays.csv') -> None:
     fight_delays = pd.read_csv(flight_path, encoding='cp1251')
-    return fight_delays
+    table1 =  fight_delays
 
 
 '''
 2) Загружает данные из файла Month.csv
 '''
-def load_moths(months_path:str='Month.csv') -> pd.DataFrame:
+def load_moths(months_path:str='Month.csv') -> None:
     Month_table = pd.read_csv(months_path, header=None, encoding='cp1251', sep=';', names = ['code', 'Month'])
-    return Month_table
+    table2 =  Month_table
 
 
 '''
 3) Выполняет merge по столбцам Month и month_code
 '''
-def merge_data_by_months(flight_path:str='flight_delays.csv', months_path:str='Month.csv') -> pd.DataFrame:
-    fight_delays = load_fights(flight_path)
-    Month_table = load_moths(months_path)
+def merge_data_by_months() -> None:
+    fight_delays = table1
+    Month_table = table2
     merged_df = fight_delays.merge(Month_table, left_on='Month', right_on='code', how='left')
     merged_df.Month_x = merged_df.Month_y
     merged_df.drop(['code', 'Month_y'], axis=1, inplace=True)
     merged_df.rename(columns={'Month_x': 'Month'}, inplace=True)
-    return merged_df
+    merged_table = merged_df
 
 
 '''
 4) Вывести результат функцией print
 '''
-def print_table(flight_path:str='flight_delays.csv', months_path:str='Month.csv') -> None:
-    print(merge_data_by_months(flight_path, months_path))
+def print_table() -> None:
+    print(merged_table)
 
 
-'''
-5) сохранение результата в формате XML в каталог на сервере Airflow
-'''
-def convert_merged_to_xml(flight_path:str='flight_delays.csv', months_path:str='Month.csv') -> None:
-    merge_data_by_months(flight_path, months_path).to_xml('merged_df.xml') ## сохранится там же, где и скрипт .py
-
-
+def convert_merged_to_xml() -> None:
+    merged_table.to_xml('merged_df.xml') ## сохранится там же, где и скрипт .py так как я не разобрался с файловой системой airflow
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 12, 12)
+    'start_date': datetime(2023, 12, 12),
+    'retries': 1
 }
 
-dag = DAG('flight_delay_analysis_xml', default_args=default_args, schedule=timedelta(days=1))
+dag = DAG('flight_delay_analysis', default_args=default_args, schedule=timedelta(days=1))
 
 load_flight_delays_task = PythonOperator(
     task_id='load_flight_delays_data',
     python_callable=load_fights,
     dag=dag
 )
+
 
 load_month_data_task = PythonOperator(
     task_id='load_month_data',
@@ -93,7 +94,6 @@ print_result_task = PythonOperator(
     dag=dag
 )
 
-
 convert_xml_task = PythonOperator(
     task_id='save_xml_result',
     python_callable=convert_merged_to_xml,
@@ -101,11 +101,9 @@ convert_xml_task = PythonOperator(
 )
 
 
-
 '''
 описываю поток
 '''
-
 load_flight_delays_task >> merge_data_task
 load_month_data_task >> merge_data_task
 merge_data_task >> print_result_task
